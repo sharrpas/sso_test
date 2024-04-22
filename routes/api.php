15c2e4f6088d853fd45a7b8811b466da347c8aa3
Sina <sharrpas@gmail.com>
2024-04-22 15:51:40 +0330

    feat: google login added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/google/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('google-login',function (){
+
+    $s_user = Socialite::driver('google')->user();
+
+    $user = User::firstOrCreate(
+        ['email' => $s_user->email],
+        [
+            'name' => $s_user->name,
+            'password' => Hash::make(Str::random(8))
+        ]
+    );
+
+    Auth::login($user, true);
+
+    dd(Auth::user());
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
